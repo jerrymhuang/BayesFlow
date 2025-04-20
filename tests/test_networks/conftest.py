a814aa53722f7e032f575c0a9011cce2f81c9487
Valentin Pratz <git@valentinpratz.de>
2025-04-20 08:52:42 +0000

    Add serialization and tests for FusionTransformer

--- tests/test_networks/conftest.py
+++ tests/test_networks/conftest.py
@@ -127,6 +127,13 @@ def time_series_transformer(summary_dim):
 
 
 @pytest.fixture(scope="function")
+def fusion_transformer(summary_dim):
+    from bayesflow.networks import FusionTransformer
+
+    return FusionTransformer(summary_dim=summary_dim)
+
+
+@pytest.fixture(scope="function")
 def set_transformer(summary_dim):
     from bayesflow.networks import SetTransformer
 
@@ -141,7 +148,15 @@ def deep_set(summary_dim):
 
 
 @pytest.fixture(
-    params=[None, "time_series_network", "time_series_transformer", "set_transformer", "deep_set"], scope="function"
+    params=[
+        None,
+        "time_series_network",
+        "time_series_transformer",
+        "fusion_transformer",
+        "set_transformer",
+        "deep_set",
+    ],
+    scope="function",
 )
 def summary_network(request, summary_dim):
     if request.param is None:
