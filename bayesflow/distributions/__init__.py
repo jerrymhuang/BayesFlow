7ce37cfa19f8e6dba553c71e37dba5e7924fb056
stefanradev93 <stefan.radev93@gmail.com>
2025-04-25 15:22:34 -0400

    Fix zombie find_distribution import

--- bayesflow/distributions/__init__.py
+++ bayesflow/distributions/__init__.py
@@ -9,8 +9,6 @@ from .diagonal_normal import DiagonalNormal
 from .diagonal_student_t import DiagonalStudentT
 from .mixture import Mixture
 
-from .find_distribution import find_distribution
-
 from ..utils._docs import _add_imports_to_all
 
 _add_imports_to_all(include_modules=[])
